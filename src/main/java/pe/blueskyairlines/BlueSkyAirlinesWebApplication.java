668e0daf076f555bfe24a67002993c402c1f4d9e
DJFM2620 <joel2620@hotmail.es>
2022-08-29 09:15:35 -0500

    Creacion del proyecto de Aerolinea

---
+++ src/main/java/pe/blueskyairlines/BlueSkyAirlinesWebApplication.java
@@ -0,0 +1,13 @@
+package pe.blueskyairlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueSkyAirlinesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueSkyAirlinesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/blueskyairlines/BlueSkyAirlinesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.blueskyairlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueSkyAirlinesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
